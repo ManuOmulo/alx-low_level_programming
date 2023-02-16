67df73763b8ee1cebfa2b22151c1b7cd4a08246c
ManuOmulo <omuloemmanuel@gmail.com>
2023-02-16 14:23:21 -0800

    Finished extra work

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unist.h>
+/**
+ *Description: main - funtions that outpust without puts and mother
+  Return: 1 for success
+  */
+int main(void)
+{
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
+
