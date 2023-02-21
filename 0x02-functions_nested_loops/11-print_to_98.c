5b90da5d3425658c7376689237f93a3718cd2b77
ManuOmulo <omuloemmanuel@gmail.com>
2023-02-21 14:18:01 -0800

    Check #11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - rints all natural numbers from n to 98
+ * @n: parameter to be checked
+ * Return: (0) is success
+ */
+
+void print_to_98(int n)
+{
+int i;
+if (n <= 98)
+{
+for (i = n; i <= 98; i++)
+{
+printf("%d, ", i);
+}
+printf("\n");
+}
+else
+{
+for (i = n; i >= 98; i--)
+{
+printf("%d, ", i);
+}
+printf("\n");
+}
+}
