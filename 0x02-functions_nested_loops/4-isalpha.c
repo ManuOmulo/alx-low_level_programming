1ca590d72f1cca3a62acd1a95795241d79133276
ManuOmulo <omuloemmanuel@gmail.com>
2023-02-21 11:58:11 -0800

    Check #4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic characters
+ * @c: parameter to be checked
+ * Return: (1) if is letter, lowercase or uppercase
+ *		otherwise return (0)
+ */
+
+int _isalpha(int c)
+{
+int var;
+if (c >= 65 && c <= 90)
+{
+var = 1;
+return (var);
+}
+else if (c >= 97 && <= 122)
+{
+var = 1;
+return (var);
+}
+else
+{
+var = 0;
+return (var);
+}
+}
+
