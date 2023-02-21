c3eb7259535e45d1ab790bef42ae89af03e17beb
ManuOmulo <omuloemmanuel@gmail.com>
2023-02-21 14:37:08 -0800

    Check #11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,7 +14,14 @@ if (n <= 98)
 {
 for (i = n; i <= 98; i++)
 {
+if (i != 98)
+{
 printf("%d, ", i);
+}
+else
+{
+printf("%d", i);
+}
 }
 printf("\n");
 }
@@ -22,7 +29,14 @@ else
 {
 for (i = n; i >= 98; i--)
 {
+if (i != 98)
+{
 printf("%d, ", i);
+}
+else
+{
+printf("%d", i);
+}
 }
 printf("\n");
 }
