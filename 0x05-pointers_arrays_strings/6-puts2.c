08ac568f679e204f7dd282be1a2cf5ed3d26a9b7
ManuOmulo <omuloemmanuel@gmail.com>
2023-02-27 12:05:23 -0800

    Recheck #6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * puts2 - prints every other character of a string, starting 
+ * puts2 - prints every other character of a string, starting
  * with the first character
  * @str: parameter to be modified
  * Return: void
