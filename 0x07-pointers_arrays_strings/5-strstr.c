59df8f67983c98d38516699adbf8352443f97d92
ManuOmulo <omuloemmanuel@gmail.com>
2023-03-06 13:18:20 -0800

    Check #7

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+* _strstr - locates a substring
+* @haystack: input
+* @needle: input
+* Return: pointer or NULL
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+for (; *haystack != '\0'; haystack++)
+{
+char *l = haystack;
+char *p = needle;
+
+while (*l == *p && *p != '\0')
+{
+l++;
+p++;
+}
+
+if (*p == '\0')
+return (haystack);
+}
+
+return (0);
+}
+
