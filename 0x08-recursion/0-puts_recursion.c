468ac619fc729a8b4fe759e96d1e0346871fb02f
ManuOmulo <omuloemmanuel@gmail.com>
2023-03-08 09:28:17 -0800

    Check #0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string
+ * @s: string to be printed
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	int i = 0;
+
+	if (s[i] == '\0')
+		return;
+	_putchar(s[i]);
+	_puts_recursion(s + 1);
+}
+
